fac23992dab96276f36bff50fd688df9cc8ad6ba
Abhilash Knox George <user.abhilash@gmail.com>
2022-11-23 16:22:04 +0530

    Initial Commit

---
+++ NBA Player Information/NBA_Player_InformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_Player_InformationApp.swift
+//  NBA Player Information
+//
+//  Created by Abhilash k George on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NBA_Player_InformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
